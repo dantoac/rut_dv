4c570012e6897e65fd82a3baeb5de489ee0fc317
Danto AC <dantoac@lavabit.com>
2012-03-14 23:23:39 -0300

    init

---
+++ rut_dv.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Calculo del Digito Validador del RUT (Rol Único Tributario, Chile). 
+Compatible con Python2 y Python3
+
+Licencia: Public Domain
+Author: Daniel Aguayo Catalan <daniel_aguayo_catalan@lavabit.com>
+
+El siguiente código se puede reducir eliminando las partes de
+intención didáctica.
+"""
+
+# El RUT como STRING desde entrada por teclado 
+rut_number = str(eval(repr(input())))
+
+rut = [number for number in rut_string][:8]# usa sólo los 8 primeros caracteres
+
+def rut_dv(rut):
+    val = 2*list(range(4,10)[::-1])[:8] #val = [9,8,7,6,5,4,9,8]
+    rut.reverse()
+    dv = sum([int(r)*n for r,n in zip(rut,val)]) % 11
+    if dv == 10: dv = 'k'
+    return(dv)
+
+print(rut_dv(rut))
